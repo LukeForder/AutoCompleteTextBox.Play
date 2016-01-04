b5fa0df11ea0ceff2b9fcfbc25e918e9df30e901
Luke Forder <LukeForder@users.noreply.github.com>
2016-01-04 20:44:07 +0000

    a

---
+++ AutoCompleteControls/CountriesProvider.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace AutoCompleteControls
+{
+    public class CountriesProvider
+    {
+        public async Task<IEnumerable<Country>> GetCountriesAsync(string searchTerm)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var request = await client.GetAsync("https://restcountries.eu/rest/v1/all");
+
+                var json = await request.Content.ReadAsStringAsync();
+
+                var countries = JsonConvert.DeserializeObject<IEnumerable<Country>>(json, new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
+
+                return countries.Where(x => x.Name?.StartsWith(searchTerm) ?? true);
+            }
+        }
+    }
+}
---
+++ AutoCompleteControls/MainVM.cs
@@ -0,0 +1,19 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfControls;
+
+namespace AutoCompleteControls
+{
+    [ImplementPropertyChanged]
+    public class MainVM 
+    {
+        public string Text { get; set; }
+
+        public ISuggestionProvider AutoCompleteSuggestions => new SuggestionProvider(searchTerm => new CountriesProvider().GetCountriesAsync(searchTerm).Result);
+    }
+}
